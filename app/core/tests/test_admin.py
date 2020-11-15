0c5d7bf3315fdc20692d083494e9663c5ec5b1af
Shawn Pike <pike.shawn@gmail.com>
2020-11-15 08:57:01 -0700

    updated admin to support custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,52 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='testuser@test.com',
+            password='pasword123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user change works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+    # def calculate_statistics(self, purchases):
+    #     average_purchase = sum(purchases) / len(purchases)
+    #     largest_purchase = max(purchases)
+    #     return average_purchase, largest_purchase
+    #
+    # def test_sequences_list(self):
+    #     purchases = [2.30, 3.90, 4.60, 7.80, 2.20, 2.40, 8.30]
+    #     average, largest = self.calculate_statistics(purchases)
+    #     print(average, largest)
